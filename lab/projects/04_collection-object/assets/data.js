41783db87e47f2dc502696d1a333089109121d40
jordantrimble <trimj250@newschool.edu>
2024-03-08 02:00:44 -0500

    commit

---
+++ lab/projects/04_collection-object/assets/data.js
@@ -0,0 +1,93 @@
+// An array of objects representing my collection:
+var data = [
+    {
+        animal: 'rat',
+        date: 'october 6th, 2022',
+        location: '301 dorms',
+        reason: 'ran at my feet at 9:25 pm because i shook a bag of chips',
+    },
+    {
+        animal: 'pigeon',
+        date: 'april 10th, 2023',
+        location: 'washington square park',
+        reason: 'he was strutting! he looked like he had places to be',
+    },
+    {
+        animal: 'cat',
+        date: 'august 27th, 2023',
+        location: '8th ave',
+        reason: 'nibbled my brothers hand. i thought that was funny',
+    },
+    {
+        animal: 'dogs',
+        date: 'september 13th, 2023',
+        location: '5th ave',
+        reason: 'one dog was napping in the middle of the sidewalk and the other was sitting next to the owner',
+    },
+    {
+        animal: 'dog',
+        date: 'september 20th, 2023',
+        location: 'parsons',
+        reason: 'my friend and i were waiting to cross the street and this dog sat there and stared at us until its owner started walking',
+    },
+    {
+        animal: 'dog',
+        date: 'november 4th, 2023',
+        location: 'union square',
+        reason: 'this very sweet dog was sitting next to its owners farmers market stand. i sat with it for like ten minutes',
+    },
+    {
+        animal: 'geese',
+        date: 'nevember 5th, 2023',
+        location: 'central park',
+        reason: 'they were swimming in circles when all of a sudden one of them stuck its head in the water',
+    },
+    {
+        animal: 'dog',
+        date: 'january 28th, 2024',
+        location: 'sephora',
+        reason: 'i took this photo because this dog just straight up looked like a wolf',
+    },
+    {
+        animal: 'dog',
+        date: 'february 3rd, 2024',
+        location: 'blank street coffee',
+        reason: 'very politely sat and waited for its owners drink',
+    },
+    {
+        animal: 'dog',
+        date: 'february 7th, 2024',
+        location: 'parsons',
+        reason: 'stood in one spot for a minute and didnt move a muscle',
+    },
+    {
+        animal: 'pigeon',
+        date: 'february 9th, 2024',
+        location: '8th ave',
+        reason: 'was laying on its side and not moving so i thought it was dead. as i got closer he started to move again',
+    },
+    {
+        animal: 'dog',
+        date: 'february 11th, 2024',
+        location: 'st. george cafe',
+        reason: 'was trying his best to behave but got too excited and started to bark. he was a puppy',
+    },
+    {
+        animal: 'chameleon',
+        date: 'february 29th, 2024',
+        location: 'petco',
+        reason: 'strutted over to the glass and started dancing',
+    },
+    {
+        animal: 'dog',
+        date: 'march 1sr, 2024',
+        location: 'blank street coffee',
+        reason: 'very politely greeted a man and then made his way over to me to say hi. after he said hi he returned to his owner and sat down',
+    },
+    {
+        animal: 'pigeon',
+        date: 'march 3rd, 2024',
+        location: 'chelsea market',
+        reason: 'i was eating bagels with my friend and this pigeon landed on our table and started walking towards us with too much confidence',
+    },
+];
