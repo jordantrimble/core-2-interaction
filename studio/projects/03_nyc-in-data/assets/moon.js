98f166a87efac43c9e1d6432c6abbf7a84713d2e
jordantrimble <trimj250@newschool.edu>
2024-05-03 12:52:01 -0400

    commit

--- studio/projects/03_nyc-in-data/assets/moon.js
+++ studio/projects/03_nyc-in-data/assets/moon.js
@@ -1,4 +1,6 @@
-var URL = 'https://moon-phase.p.rapidapi.com/basic'
+var lat = 40.7
+var lon = -73.9
+var URL = `https://moon-phase.p.rapidapi.com/advanced?lat=${lat}&lon=${lon}`;
 var key = 'fb17a5b661mshd65f0506b9963c2p1e4e6djsn1174d5435dae';
 
 fetch(URL, {
@@ -13,4 +15,14 @@ fetch(URL, {
     })
     .then((data)=>{
         console.log(data);
+        var backgroundElement = document.querySelector(".background");
+        if (data.moon.stage == "waxing") {
+            // waxing animation
+            backgroundElement.style.right = "0";
+            backgroundElement.style.width = `${data.moon.phase * 100}%`;
+        } else if (data.moon.stage == "waning") {
+            // waning animation
+            backgroundElement.style.left = "0";
+            backgroundElement.style.width = `${(1 - data.moon.phase) * 100}%`;
+        }
     })
